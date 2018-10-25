7c04df8c37c0e32f859a6a6a72bd7c20d0eb0c33
peter <peter@xulukun.cn>
2018-10-25 23:29:25 +0800

    :wrench: Update

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -101,7 +101,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'https://github.com/pointline/resume' // 这里注意改成你的在线简历的网站
+        'https://pointline.github.io/resume/' // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
